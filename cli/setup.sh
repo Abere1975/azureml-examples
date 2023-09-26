1ea91daefef641f6aafe4aadbc271f6ccf901646
Abere1975 <120916183+Abere1975@users.noreply.github.com>
2023-09-26 18:19:58 +0100

    Update setup.sh
    changes made to group. location and workspace.

--- cli/setup.sh
+++ cli/setup.sh
@@ -25,9 +25,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
+GROUP="projecteasy_visaml"
 LOCATION="eastus"
-WORKSPACE="main"
+WORKSPACE="azureml"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
